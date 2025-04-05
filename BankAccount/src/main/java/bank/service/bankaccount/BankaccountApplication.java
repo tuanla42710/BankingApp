8bdeef3a437231e1a999e992785b543319c34e6e
tuanla42710 <leanhtuanlmh@gmail.com>
2025-04-05 09:53:41 +0700

    add new file

---
+++ BankAccount/src/main/java/bank/service/bankaccount/BankaccountApplication.java
@@ -0,0 +1,13 @@
+package bank.service.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaccountApplication.class, args);
+	}
+
+}
